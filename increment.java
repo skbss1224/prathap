c56068f996286af7415914c4880b5249b1b5892b
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-06-30 16:52:54 +0530

    arithmetic,relational
    unery

---
+++ increment.java
@@ -0,0 +1,85 @@
+package prathap;
+
+import java.util.Scanner;
+
+public class increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//arithmetic operators
+		//plus operator-->+
+		//minus operator-->-
+		//mul operator-->*
+		//div operator -->/
+		//per operator -->%
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("enter the A value:");
+//		int a=scan.nextInt();
+//		System.out.println("enter the b value:");
+//		int b=scan.nextInt();
+//		int c;
+//		c=a+b;
+//		System.out.println("plus operator:"+c);
+//		c=a-b;
+//		System.out.println("minus operator:"+c);
+//		c=a*b;
+//		System.out.println("mul operator:"+c);
+//		c=a/b;
+//		System.out.println("div operator:"+c);
+//		c=a%b;
+//		System.out.println("per operator:"+c);
+		
+		//relational operator
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("enter the A value:");
+//		int a=scan.nextInt();
+//		System.out.println("enter the b value:");
+//		int b=scan.nextInt();
+//		System.out.println("assignment operator");
+//		System.out.println(a==b);//equal operator
+//		System.out.println("greater than operator");
+//
+//		System.out.println(a>b);
+//		System.out.println("less than operator");
+//
+//		System.out.println(a<b);
+//		System.out.println("greater than equal operator");
+//
+//		System.out.println(a>=b);
+//		System.out.println("less than equal operator");
+//
+//		System.out.println(a<=b);
+		
+		
+		//unery operator
+		int i=5,j=4;
+//		System.out.println(i++);//post increment
+//		System.out.println(i);
+//		System.out.println(i++);
+//		System.out.println(i);
+//		System.out.println(++i);//pre increment
+//		System.out.println(i);
+//		System.out.println(++i);
+//		System.out.println(i);
+		
+//		System.out.println(--i);//pre degrement
+//		System.out.println(i);
+//		System.out.println(--i);
+//		System.out.println(i);
+		
+//		System.out.println(i--);//post degrement
+//		System.out.println(i);
+//		System.out.println(i--);
+//		System.out.println(i);
+		System.out.println(++j+j+++ ++i+ i++);
+						//(5+5+6+6)
+						//22
+		System.out.println(++j - i+++ --j -++i);
+						//(5-5+4-7)
+						//(-3)
+		
+		
+		
+	}
+
+}
